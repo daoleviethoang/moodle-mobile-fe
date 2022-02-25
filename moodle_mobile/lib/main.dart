b3ec15527915ec9bfa8d3c20ce1ab3ca79b789f4
thanhthao1401 <nttthao18@clc.fitus.edu.vn>
2022-02-25 23:16:23 +0700

    Add login page and project simple structure

--- moodle_mobile/lib/main.dart
+++ moodle_mobile/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:moodle_mobile/constants/colors.dart';
+import 'package:moodle_mobile/screens/login.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,37 +9,30 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Moodle App',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Text('Hello World'),
-      ),
+          colorScheme: const ColorScheme(
+        brightness: Brightness.light,
+        primary: MoodleColors.blue,
+        secondary: MoodleColors.blue,
+        background: Colors.white,
+        // Need to consider again
+        primaryVariant: Colors.black,
+        secondaryVariant: Colors.black,
+        surface: Colors.black,
+        error: Colors.black,
+        onPrimary: Colors.black,
+        onSecondary: Colors.black,
+        onSurface: Colors.black,
+        onBackground: Colors.black,
+        onError: Colors.black,
+      )),
+      home: const LoginScreen(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
