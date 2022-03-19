0ee13f3f7b517e3fd47c453ac8961c71bbdb2d3a
daoleviethoang <47211985+daoleviethoang@users.noreply.github.com>
2022-03-19 17:44:58 +0700

    handle click to course move to course detail

--- moodle_mobile/lib/screens/home/courses_view.dart
+++ moodle_mobile/lib/screens/home/courses_view.dart
@@ -85,119 +85,254 @@ class CategoryView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding:
-          const EdgeInsets.only(left: 0.0, top: 0.0, right: 0.0, bottom: 10.0),
-      child: Container(
-        height: 166,
-        decoration: BoxDecoration(
-          boxShadow: [
-            BoxShadow(
-              color: Colors.grey.withOpacity(0.5),
-              spreadRadius: 5,
-              blurRadius: 5,
-              offset: const Offset(0, 3), // changes position of shadow
-            ),
-          ],
-          gradient: const LinearGradient(
-              begin: Alignment.topRight,
-              stops: [0.1, 0.02],
-              colors: [MoodleColors.blue, Colors.white]),
-          borderRadius: const BorderRadius.all(const Radius.circular(8.0)),
-        ),
-        child: Container(
-          margin: const EdgeInsets.only(
-              left: 26.0, top: 13.0, right: 10.0, bottom: 0.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: <Widget>[
-                  Container(
-                    height: 26,
-                    width: 54,
-                    decoration: BoxDecoration(
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.grey.withOpacity(0.5),
-                          spreadRadius: 2,
-                          blurRadius: 2,
-                          offset:
-                              const Offset(0, 2), // changes position of shadow
-                        ),
-                      ],
-                      borderRadius:
-                          const BorderRadius.all(Radius.circular(3.0)),
-                      color: MoodleColors.tagColor,
-                    ),
-                    child: Center(
-                      child: Text(
-                        '${category!.tag}',
-                        textAlign: TextAlign.left,
-                        style: const TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 12,
-                            letterSpacing: 0.27,
-                            color: MoodleColors.blue),
-                      ),
-                    ),
-                  ),
-                  IconButton(
-                    iconSize: 30,
-                    icon: const Icon(Icons.more_vert),
-                    color: MoodleColors.black,
-                    onPressed: () {},
-                  ),
-                ],
-              ),
-              Padding(
+    return AnimatedBuilder(
+      animation: animationController!,
+      builder: (BuildContext context, Widget? child) {
+        return FadeTransition(
+          opacity: animation!,
+          child: Transform(
+            transform: Matrix4.translationValues(
+                0.0, 50 * (1.0 - animation!.value), 0.0),
+            child: InkWell(
+              splashColor: Colors.transparent,
+              onTap: callback,
+              child: Padding(
                 padding: const EdgeInsets.only(
-                    left: 0.0, top: 4.0, right: 0.0, bottom: 15.0),
-                child: Text(
-                  '${category!.title}',
-                  textAlign: TextAlign.left,
-                  style: const TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 16,
-                      letterSpacing: 0.27,
-                      color: MoodleColors.black),
+                    left: 0.0, top: 0.0, right: 0.0, bottom: 10.0),
+                child: Container(
+                  height: 166,
+                  decoration: BoxDecoration(
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.5),
+                        spreadRadius: 5,
+                        blurRadius: 5,
+                        offset:
+                            const Offset(0, 3), // changes position of shadow
+                      ),
+                    ],
+                    gradient: const LinearGradient(
+                        begin: Alignment.topRight,
+                        stops: [0.1, 0.02],
+                        colors: [MoodleColors.blue, Colors.white]),
+                    borderRadius:
+                        const BorderRadius.all(const Radius.circular(8.0)),
+                  ),
+                  child: Container(
+                    margin: const EdgeInsets.only(
+                        left: 26.0, top: 13.0, right: 10.0, bottom: 0.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: <Widget>[
+                            Container(
+                              height: 26,
+                              width: 54,
+                              decoration: BoxDecoration(
+                                boxShadow: [
+                                  BoxShadow(
+                                    color: Colors.grey.withOpacity(0.5),
+                                    spreadRadius: 2,
+                                    blurRadius: 2,
+                                    offset: const Offset(
+                                        0, 2), // changes position of shadow
+                                  ),
+                                ],
+                                borderRadius: const BorderRadius.all(
+                                    Radius.circular(3.0)),
+                                color: MoodleColors.tagColor,
+                              ),
+                              child: Center(
+                                child: Text(
+                                  '${category!.tag}',
+                                  textAlign: TextAlign.left,
+                                  style: const TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 12,
+                                      letterSpacing: 0.27,
+                                      color: MoodleColors.blue),
+                                ),
+                              ),
+                            ),
+                            IconButton(
+                              iconSize: 30,
+                              icon: const Icon(Icons.more_vert),
+                              color: MoodleColors.black,
+                              onPressed: () {},
+                            ),
+                          ],
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(
+                              left: 0.0, top: 4.0, right: 0.0, bottom: 15.0),
+                          child: Text(
+                            '${category!.title}',
+                            textAlign: TextAlign.left,
+                            style: const TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 16,
+                                letterSpacing: 0.27,
+                                color: MoodleColors.black),
+                          ),
+                        ),
+                        Column(
+                            children: List<Widget>.generate(
+                          category!.teacher.length,
+                          (int index) {
+                            final int count = Course.popularCourseList.length;
+                            final Animation<double> animation =
+                                Tween<double>(begin: 0.0, end: 1.0).animate(
+                              CurvedAnimation(
+                                parent: animationController!,
+                                curve: Interval((1 / count) * index, 1.0,
+                                    curve: Curves.fastOutSlowIn),
+                              ),
+                            );
+                            animationController?.forward();
+                            return Padding(
+                                padding: const EdgeInsets.only(
+                                    left: 0.0,
+                                    top: 0.0,
+                                    right: 0.0,
+                                    bottom: 5.0),
+                                child: Text(
+                                  'Teacher: ' + category!.teacher[index],
+                                  textAlign: TextAlign.left,
+                                  style: const TextStyle(
+                                      fontWeight: FontWeight.normal,
+                                      fontSize: 11,
+                                      letterSpacing: 0.27,
+                                      color: MoodleColors.black),
+                                ));
+                          },
+                        ))
+                      ],
+                    ),
+                  ),
                 ),
               ),
-              Column(
-                  children: List<Widget>.generate(
-                category!.teacher.length,
-                (int index) {
-                  final int count = Course.popularCourseList.length;
-                  final Animation<double> animation =
-                      Tween<double>(begin: 0.0, end: 1.0).animate(
-                    CurvedAnimation(
-                      parent: animationController!,
-                      curve: Interval((1 / count) * index, 1.0,
-                          curve: Curves.fastOutSlowIn),
-                    ),
-                  );
-                  animationController?.forward();
-                  return Padding(
-                      padding: const EdgeInsets.only(
-                          left: 0.0, top: 0.0, right: 0.0, bottom: 5.0),
-                      child: Text(
-                        'Teacher: ' + category!.teacher[index],
-                        textAlign: TextAlign.left,
-                        style: const TextStyle(
-                            fontWeight: FontWeight.normal,
-                            fontSize: 11,
-                            letterSpacing: 0.27,
-                            color: MoodleColors.black),
-                      ));
-                },
-              ))
-            ],
+            ),
           ),
-        ),
-      ),
+        );
+      },
     );
+    // return Padding(
+    //   padding:
+    //       const EdgeInsets.only(left: 0.0, top: 0.0, right: 0.0, bottom: 10.0),
+    //   child: Container(
+    //     height: 166,
+    //     decoration: BoxDecoration(
+    //       boxShadow: [
+    //         BoxShadow(
+    //           color: Colors.grey.withOpacity(0.5),
+    //           spreadRadius: 5,
+    //           blurRadius: 5,
+    //           offset: const Offset(0, 3), // changes position of shadow
+    //         ),
+    //       ],
+    //       gradient: const LinearGradient(
+    //           begin: Alignment.topRight,
+    //           stops: [0.1, 0.02],
+    //           colors: [MoodleColors.blue, Colors.white]),
+    //       borderRadius: const BorderRadius.all(const Radius.circular(8.0)),
+    //     ),
+    //     child: Container(
+    //       margin: const EdgeInsets.only(
+    //           left: 26.0, top: 13.0, right: 10.0, bottom: 0.0),
+    //       child: Column(
+    //         mainAxisAlignment: MainAxisAlignment.start,
+    //         crossAxisAlignment: CrossAxisAlignment.start,
+    //         children: <Widget>[
+    //           Row(
+    //             mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    //             crossAxisAlignment: CrossAxisAlignment.center,
+    //             children: <Widget>[
+    //               Container(
+    //                 height: 26,
+    //                 width: 54,
+    //                 decoration: BoxDecoration(
+    //                   boxShadow: [
+    //                     BoxShadow(
+    //                       color: Colors.grey.withOpacity(0.5),
+    //                       spreadRadius: 2,
+    //                       blurRadius: 2,
+    //                       offset:
+    //                           const Offset(0, 2), // changes position of shadow
+    //                     ),
+    //                   ],
+    //                   borderRadius:
+    //                       const BorderRadius.all(Radius.circular(3.0)),
+    //                   color: MoodleColors.tagColor,
+    //                 ),
+    //                 child: Center(
+    //                   child: Text(
+    //                     '${category!.tag}',
+    //                     textAlign: TextAlign.left,
+    //                     style: const TextStyle(
+    //                         fontWeight: FontWeight.bold,
+    //                         fontSize: 12,
+    //                         letterSpacing: 0.27,
+    //                         color: MoodleColors.blue),
+    //                   ),
+    //                 ),
+    //               ),
+    //               IconButton(
+    //                 iconSize: 30,
+    //                 icon: const Icon(Icons.more_vert),
+    //                 color: MoodleColors.black,
+    //                 onPressed: () {},
+    //               ),
+    //             ],
+    //           ),
+    //           Padding(
+    //             padding: const EdgeInsets.only(
+    //                 left: 0.0, top: 4.0, right: 0.0, bottom: 15.0),
+    //             child: Text(
+    //               '${category!.title}',
+    //               textAlign: TextAlign.left,
+    //               style: const TextStyle(
+    //                   fontWeight: FontWeight.bold,
+    //                   fontSize: 16,
+    //                   letterSpacing: 0.27,
+    //                   color: MoodleColors.black),
+    //             ),
+    //           ),
+    //           Column(
+    //               children: List<Widget>.generate(
+    //             category!.teacher.length,
+    //             (int index) {
+    //               final int count = Course.popularCourseList.length;
+    //               final Animation<double> animation =
+    //                   Tween<double>(begin: 0.0, end: 1.0).animate(
+    //                 CurvedAnimation(
+    //                   parent: animationController!,
+    //                   curve: Interval((1 / count) * index, 1.0,
+    //                       curve: Curves.fastOutSlowIn),
+    //                 ),
+    //               );
+    //               animationController?.forward();
+    //               return Padding(
+    //                   padding: const EdgeInsets.only(
+    //                       left: 0.0, top: 0.0, right: 0.0, bottom: 5.0),
+    //                   child: Text(
+    //                     'Teacher: ' + category!.teacher[index],
+    //                     textAlign: TextAlign.left,
+    //                     style: const TextStyle(
+    //                         fontWeight: FontWeight.normal,
+    //                         fontSize: 11,
+    //                         letterSpacing: 0.27,
+    //                         color: MoodleColors.black),
+    //                   ));
+    //             },
+    //           ))
+    //         ],
+    //       ),
+    //     ),
+    //   ),
+    // );
   }
 }
