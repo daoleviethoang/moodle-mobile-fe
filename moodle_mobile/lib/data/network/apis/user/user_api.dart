8b83869768c050d2a0490c1679bc192c18789b77
HaTheHien <47638822+HaTheHien@users.noreply.github.com>
2022-04-19 13:40:07 +0700

    fix dio not found when reload flutter

--- moodle_mobile/lib/data/network/apis/user/user_api.dart
+++ moodle_mobile/lib/data/network/apis/user/user_api.dart
@@ -1,8 +1,10 @@
 import 'dart:async';
 import 'dart:convert';
 
+import 'package:dio/dio.dart';
 import 'package:moodle_mobile/data/network/constants/endpoints.dart';
 import 'package:moodle_mobile/data/network/dio_client.dart';
+import 'package:moodle_mobile/data/network/dio_http.dart';
 import 'package:moodle_mobile/models/user.dart';
 
 class UserApi {
@@ -15,17 +17,18 @@ class UserApi {
   /// Returns list of post in response
   Future<String> login(String username, String password, String service) async {
     try {
-      final res = await _dioClient.get(Endpoints.login, queryParameters: {
+      Dio dio = Http().client;
+      final res = await dio.get(Endpoints.login, queryParameters: {
         'username': username,
         'password': password,
         'service': service
       });
 
-      if (res['token'] == null) {
+      if (res.data['token'] == null) {
         throw Exception('Wrong username or password, please try again!');
       }
 
-      return res['token'];
+      return res.data['token'];
     } catch (e) {
       rethrow;
     }
@@ -34,30 +37,22 @@ class UserApi {
   // Write a future return user info
   Future<UserModel> getUserInfo(String token, String username) async {
     try {
-      final res =
-          await _dioClient.get(Endpoints.webserviceServer, queryParameters: {
+      Dio dio = Http().client;
+      var res = await dio.get(Endpoints.webserviceServer, queryParameters: {
         'wstoken': token,
         'wsfunction': 'core_user_get_users_by_field',
         'moodlewsrestformat': 'json',
         'field': 'username',
-        'values[0]': username
+        'values[]': [username]
       });
       print("here user api");
-      // Handle error
-      if (res.runtimeType == [].runtimeType) {
-        if (res.length == 0) {
-          throw Exception('Can not found user info!');
-        }
-      } else {
-        throw Exception(res['message']);
-      }
 
       return UserModel(
-          id: res[0]['id'],
+          id: res.data[0]['id'],
           token: token,
-          username: res[0]['username'],
-          fullname: res[0]['fullname'],
-          email: res[0]['email']);
+          username: res.data[0]['username'],
+          fullname: res.data[0]['fullname'],
+          email: res.data[0]['email']);
     } catch (e) {
       print("Error here");
       rethrow;
