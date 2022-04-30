c6481aac445a5e803c3bba2fc64392e6bd4fa3d2
HaTheHien <47638822+HaTheHien@users.noreply.github.com>
2022-04-30 09:17:16 +0700

    file upload, add post in forum with file

--- moodle_mobile/lib/data/network/apis/file/file_api.dart
+++ moodle_mobile/lib/data/network/apis/file/file_api.dart
@@ -8,24 +8,55 @@ import 'package:moodle_mobile/data/network/constants/endpoints.dart';
 import 'package:moodle_mobile/data/network/dio_http.dart';
 import 'package:path_provider/path_provider.dart' as pathProvider;
 import 'package:permission_handler/permission_handler.dart';
+import 'package:moodle_mobile/models/assignment/file_assignment.dart';
 
 class FileApi {
-  Future<int> uploadFile(String token, String filePath, int? itemid) async {
+  Future<int> uploadFile(
+      String token, String filePath, String? fileName, int? itemid) async {
     try {
       Dio dio = Http().client;
-      final res = await dio.post(Endpoints.uploadFile, queryParameters: {
+      Response<dynamic> res;
+      var formData = FormData.fromMap({
+        'file': await MultipartFile.fromFile(filePath, filename: fileName),
+      });
+      Map<String, Object> query = {
         'token': token,
         'filearea': 'draft',
-        'itemid': itemid,
-      }, data: {
-        'file': await MultipartFile.fromFile(filePath),
-      });
-      if (res.data["error"] != null) {
-        throw res.data["error"];
+      };
+      if (itemid != null) {
+        query = {
+          'token': token,
+          'filearea': 'draft',
+          'itemid': itemid,
+        };
       }
-      return res.data["itemid"];
+      res = await dio.post(Endpoints.uploadFile,
+          queryParameters: query, data: formData);
+
+      var data = jsonDecode(res.data);
+      if (data[0]["error"] != null) {
+        throw data[0]["error"];
+      }
+      return data[0]["itemid"];
     } catch (e) {
-      throw "Upload file fail";
+      throw "Upload file fail $fileName";
+    }
+  }
+
+  Future<int?> uploadMultipleFile(String token, List<FileUpload> files) async {
+    try {
+      if (files.isEmpty) {
+        return null;
+      }
+      int itemId = await FileApi()
+          .uploadFile(token, files.first.filepath, files.first.filename, null);
+      for (int i = 1; i < files.length; i++) {
+        await FileApi()
+            .uploadFile(token, files[i].filepath, files[i].filename, itemId);
+      }
+      return itemId;
+    } catch (e) {
+      rethrow;
     }
   }
 
