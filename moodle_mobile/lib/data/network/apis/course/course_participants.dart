d5d37091fada4271baef103d6ecbbfcbac77de84
thanhthao1401 <nttthao18@clc.fitus.edu.vn>
2022-05-18 11:35:07 +0700

    Participant in OneCourse

---
+++ moodle_mobile/lib/data/network/apis/course/course_participants.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:moodle_mobile/data/network/dio_http.dart';
+import 'dart:async';
+import 'dart:convert';
+import 'package:dio/dio.dart';
+import 'package:moodle_mobile/data/network/constants/endpoints.dart';
+import 'package:moodle_mobile/data/network/constants/wsfunction_constants.dart';
+import 'package:moodle_mobile/models/course/course_participants.dart';
+
+class CourseParticipants {
+  Future<List<CourseParticipantsModel>> getParticipantInCourse(
+    String token,
+    int courseId,
+  ) async {
+    try {
+      Dio dio = Http().client;
+      final res = await dio.get(Endpoints.webserviceServer, queryParameters: {
+        'wstoken': token,
+        'wsfunction': Wsfunction.CORE_COURSES_PARTICIPANT,
+        'moodlewsrestformat': 'json',
+        'courseid': courseId,
+        'options[0][name]': 'userfields',
+        'options[0][value]': 'id,roles'
+      });
+
+      List<CourseParticipantsModel> listCourseParticipants = [];
+      for (var i = 0; i < (res.data as List).length; i++) {
+        List<RoleOfParticitpants> listRoleParticipants = [];
+
+        for (var j = 0; j < res.data[i]['roles'].length; j++) {
+          listRoleParticipants.add(RoleOfParticitpants(
+            roleID: res.data[i]['roles'][j]['roleid'],
+          ));
+        }
+
+        listCourseParticipants.add(CourseParticipantsModel(
+          id: res.data[i]['id'],
+          fullname: res.data[i]['fullname'],
+          roles: listRoleParticipants,
+        ));
+      }
+      return listCourseParticipants;
+    } catch (e) {
+      rethrow;
+    }
+  }
+}
