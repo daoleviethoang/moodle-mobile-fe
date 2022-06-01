44aa4082ae1b01e681a3ca0493e5ac962770d2a6
Nguyen Gia Hung <giahungnguyen999@gmail.com>
2022-06-01 16:19:09 +0700

    remove extra files

--- moodle_mobile/lib/data/shared_reference/constants/preferences.dart
+++ moodle_mobile/lib/data/shared_reference/constants/preferences.dart
@@ -2,4 +2,6 @@ class Preferences {
   Preferences._();
 
   static const String auth_token = "authToken";
+  static const String username = "username";
+  static const String base_url = "baseUrl";
 }
