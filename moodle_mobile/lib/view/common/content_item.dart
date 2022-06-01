b8ce97edf2477a1fd7989ff54bcb76bd37d039bf
Nguyen Gia Hung <giahungnguyen999@gmail.com>
2022-06-01 15:16:30 +0700

    make launchUrl launch external browser instead

--- moodle_mobile/lib/view/common/content_item.dart
+++ moodle_mobile/lib/view/common/content_item.dart
@@ -82,7 +82,10 @@ class DocumentItem extends StatelessWidget {
         final uri = Uri.parse(documentUrl);
         var ableLaunch = await canLaunchUrl(uri);
         if (ableLaunch) {
-          await launchUrl(uri);
+          await launchUrl(
+            uri,
+            mode: LaunchMode.externalApplication,
+          );
         } else {
           print("URL can't be launched.");
         }
@@ -143,7 +146,10 @@ class UrlItem extends StatelessWidget {
         final uri = Uri.parse(url);
         var ableLaunch = await canLaunchUrl(uri);
         if (ableLaunch) {
-          await launchUrl(uri);
+          await launchUrl(
+            uri,
+            mode: LaunchMode.externalApplication,
+          );
         } else {
           print("URL can't be launched.");
         }
@@ -370,7 +376,10 @@ class RichTextCard extends StatelessWidget {
                   TextButton(
                     onPressed: () async {
                       Navigator.pop(context);
-                      await launchUrl(Uri.parse(url ?? ''));
+                      await launchUrl(
+                        Uri.parse(url ?? ''),
+                        mode: LaunchMode.externalApplication,
+                      );
                     },
                     child: const Text('Open'),
                   ),
