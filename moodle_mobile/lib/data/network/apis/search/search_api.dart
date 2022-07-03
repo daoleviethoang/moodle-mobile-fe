c5fefab2036c5dca387649dcbc68b5ebac7dc6ab
HaTheHien <47638822+HaTheHien@users.noreply.github.com>
2022-07-04 06:11:28 +0700

    add search user

--- moodle_mobile/lib/data/network/apis/search/search_api.dart
+++ moodle_mobile/lib/data/network/apis/search/search_api.dart
@@ -6,12 +6,12 @@ import 'package:moodle_mobile/data/network/constants/wsfunction_constants.dart';
 import 'package:moodle_mobile/data/network/dio_http.dart';
 import 'package:moodle_mobile/models/contact/contact.dart';
 import 'package:moodle_mobile/models/course/courses.dart';
+import 'package:moodle_mobile/models/search_user/message_contact.dart';
 
 class SearchApi {
   // injecting dio instance
   SearchApi();
 
-  /// Returns list of post in response
   Future<List<CourseOverview>> searchCourse(String token, String query) async {
     try {
       Dio dio = Http().client;
@@ -36,6 +36,27 @@ class SearchApi {
           .toList();
     } catch (e) {
       print(e.toString());
+      rethrow;
+    }
+  }
+
+  Future<List<MessageContact>> searchUser(
+      String token, int userId, String query) async {
+    try {
+      Dio dio = Http().client;
+
+      final res = await dio.get(Endpoints.webserviceServer, queryParameters: {
+        'wstoken': token,
+        'wsfunction': Wsfunction.CORE_COURSE_SEARCH_USERS,
+        'moodlewsrestformat': "json",
+        'userid': userId,
+        "search": query,
+      });
+      var list = res.data["contacts"] as List;
+      list.addAll(res.data["noncontacts"]);
+
+      return list.map((e) => MessageContact.fromJson(e)).toList();
+    } catch (e) {
       rethrow;
     }
   }
