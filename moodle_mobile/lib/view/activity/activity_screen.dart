0039f7bd25d24c718d9f9829d4f4e80de9d11f7e
HaTheHien <47638822+HaTheHien@users.noreply.github.com>
2022-07-16 08:09:06 +0700

    add bottom dialog have camera in activity (#114)

--- moodle_mobile/lib/view/activity/activity_screen.dart
+++ moodle_mobile/lib/view/activity/activity_screen.dart
@@ -1,8 +1,12 @@
+import 'dart:io';
+
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:intl/intl.dart';
 import 'package:moodle_mobile/constants/colors.dart';
+import 'package:moodle_mobile/constants/dimens.dart';
 import 'package:moodle_mobile/data/network/apis/custom_api/custom_api.dart';
 import 'package:moodle_mobile/data/network/apis/file/file_api.dart';
 import 'package:moodle_mobile/models/assignment/file_assignment.dart';
@@ -40,6 +44,202 @@ class _ActivityScreenState extends State<ActivityScreen> {
   void initState() {
     _userStore = GetIt.instance<UserStore>();
     super.initState();
+  }
+
+  addFileFromCamera() async {
+    PickedFile? image =
+        await ImagePicker.platform.pickImage(source: ImageSource.camera);
+    if (image != null) {
+      List<FileUpload> files = [];
+
+      File file = File(image.path);
+      String fileName = file.path.split('/').last;
+      int byte = file.readAsBytesSync().lengthInBytes;
+
+      files.add(
+        FileUpload(
+          filename: fileName,
+          filepath: image.path,
+          filesize: byte,
+          timeModified: DateTime.now(),
+        ),
+      );
+
+      int? itemId =
+          await FileApi().uploadMultipleFile(_userStore.user.token, files);
+
+      // set in web
+      //int itemId = 429429131;
+
+      if (itemId != null) {
+        if (albums.any((element) =>
+            element.name == DateFormat("dd/MM/yyyy").format(DateTime.now()))) {
+          await CustomApi().addModuleFolder(
+              _userStore.user.token,
+              widget.courseId,
+              DateFormat("dd/MM/yyyy hh:mm:ss").format(DateTime.now()),
+              widget.sectionIndex,
+              itemId);
+        } else {
+          await CustomApi().addModuleFolder(
+              _userStore.user.token,
+              widget.courseId,
+              DateFormat("dd/MM/yyyy").format(DateTime.now()),
+              widget.sectionIndex,
+              itemId);
+        }
+        setState(() {
+          isLoading = true;
+        });
+        await widget.reGetContent(true);
+        setState(() {
+          isLoading = false;
+        });
+      }
+    }
+  }
+
+  addFileFromGallery() async {
+    FilePickerResult? result = await FilePicker.platform
+        .pickFiles(allowMultiple: true, type: FileType.image);
+    if (result != null) {
+      List<FileUpload> files = result.files
+          .map(
+            (e) => FileUpload(
+              filename: e.name,
+              filepath: e.path ?? "",
+              filesize: e.size,
+              timeModified: DateTime.now(),
+            ),
+          )
+          .toList();
+
+      int? itemId =
+          await FileApi().uploadMultipleFile(_userStore.user.token, files);
+
+      // set in web
+      //int itemId = 429429131;
+
+      if (itemId != null) {
+        if (albums.any((element) =>
+            element.name == DateFormat("dd/MM/yyyy").format(DateTime.now()))) {
+          await CustomApi().addModuleFolder(
+              _userStore.user.token,
+              widget.courseId,
+              DateFormat("dd/MM/yyyy hh:mm:ss").format(DateTime.now()),
+              widget.sectionIndex,
+              itemId);
+        } else {
+          await CustomApi().addModuleFolder(
+              _userStore.user.token,
+              widget.courseId,
+              DateFormat("dd/MM/yyyy").format(DateTime.now()),
+              widget.sectionIndex,
+              itemId);
+        }
+        setState(() {
+          isLoading = true;
+        });
+        await widget.reGetContent(true);
+        setState(() {
+          isLoading = false;
+        });
+      }
+    }
+  }
+
+  Widget buildBottomDialog(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.only(top: 8, bottom: 20, left: 10, right: 10),
+      decoration: const BoxDecoration(
+        color: MoodleColors.grey_bottom_bar,
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(Dimens.default_sheet_radius),
+          topRight: Radius.circular(Dimens.default_sheet_radius),
+        ),
+      ),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          Container(
+            margin: const EdgeInsets.only(bottom: 20),
+            height: 5,
+            width: 134,
+            decoration: const BoxDecoration(
+              color: MoodleColors.line_bottom_bar,
+              borderRadius: BorderRadius.all(Radius.circular(5)),
+            ),
+          ),
+          Container(
+            width: double.infinity,
+            margin: const EdgeInsets.only(bottom: 8),
+            decoration: const BoxDecoration(
+              color: MoodleColors.white,
+              borderRadius: BorderRadius.all(Radius.circular(10)),
+            ),
+            child: Padding(
+              padding:
+                  const EdgeInsets.only(left: 30, bottom: 5, top: 5, right: 30),
+              child: TextButton.icon(
+                onPressed: () async {
+                  await addFileFromGallery();
+                  Navigator.pop(context);
+                },
+                icon: const Icon(
+                  Icons.image,
+                  color: MoodleColors.black,
+                  size: 30,
+                ),
+                label: Padding(
+                  padding: const EdgeInsets.only(left: 20),
+                  child: Text(
+                    AppLocalizations.of(context)!.file_from_gallery,
+                    style: TextStyle(color: MoodleColors.black, fontSize: 16),
+                  ),
+                ),
+                style: TextButton.styleFrom(
+                  alignment: Alignment.centerLeft,
+                  primary: MoodleColors.white,
+                ),
+              ),
+            ),
+          ),
+          Container(
+            width: double.infinity,
+            decoration: const BoxDecoration(
+              color: MoodleColors.white,
+              borderRadius: BorderRadius.all(Radius.circular(10)),
+            ),
+            child: Padding(
+              padding:
+                  const EdgeInsets.only(left: 30, bottom: 5, top: 5, right: 30),
+              child: TextButton.icon(
+                onPressed: () async {
+                  await addFileFromCamera();
+                  Navigator.pop(context);
+                },
+                icon: const Icon(
+                  Icons.camera_alt,
+                  color: MoodleColors.black,
+                  size: 30,
+                ),
+                label: Padding(
+                  padding: const EdgeInsets.only(left: 20),
+                  child: Text(
+                    AppLocalizations.of(context)!.file_from_camera,
+                    style: TextStyle(color: MoodleColors.black, fontSize: 16),
+                  ),
+                ),
+                style: TextButton.styleFrom(
+                  alignment: Alignment.centerLeft,
+                  primary: MoodleColors.white,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 
   @override
@@ -98,67 +298,26 @@ class _ActivityScreenState extends State<ActivityScreen> {
                   ),
                   textScaleFactor: 1.5,
                 ),
-                widget.isTeacher == false ? Container() : IconButton(
-                    onPressed: () async {
-                      try {
-                        FilePickerResult? result = await FilePicker.platform
-                            .pickFiles(
-                                allowMultiple: true, type: FileType.image);
-                        if (result != null) {
-                          List<FileUpload> files = result.files
-                              .map(
-                                (e) => FileUpload(
-                                  filename: e.name,
-                                  filepath: e.path ?? "",
-                                  filesize: e.size,
-                                  timeModified: DateTime.now(),
-                                ),
-                              )
-                              .toList();
-
-                          int? itemId = await FileApi()
-                              .uploadMultipleFile(_userStore.user.token, files);
-
-                          // set in web
-                          //int itemId = 429429131;
-
-                          if (itemId != null) {
-                            if (albums.any((element) =>
-                                element.name ==
-                                DateFormat("dd/MM/yyyy")
-                                    .format(DateTime.now()))) {
-                              await CustomApi().addModuleFolder(
-                                  _userStore.user.token,
-                                  widget.courseId,
-                                  DateFormat("dd/MM/yyyy hh:mm:ss")
-                                      .format(DateTime.now()),
-                                  widget.sectionIndex,
-                                  itemId);
-                            } else {
-                              await CustomApi().addModuleFolder(
-                                  _userStore.user.token,
-                                  widget.courseId,
-                                  DateFormat("dd/MM/yyyy")
-                                      .format(DateTime.now()),
-                                  widget.sectionIndex,
-                                  itemId);
-                            }
-                            setState(() {
-                              isLoading = true;
-                            });
-                            await widget.reGetContent(true);
-                            setState(() {
-                              isLoading = false;
-                            });
+                widget.isTeacher == false
+                    ? Container()
+                    : IconButton(
+                        onPressed: () async {
+                          try {
+                            showModalBottomSheet(
+                              context: context,
+                              shape: const RoundedRectangleBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(20)),
+                              ),
+                              builder: (builder) => buildBottomDialog(builder),
+                            );
+                          } catch (e) {
+                            ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                                content: Text(e.toString()),
+                                backgroundColor: Colors.red));
                           }
-                        }
-                      } catch (e) {
-                        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                            content: Text(e.toString()),
-                            backgroundColor: Colors.red));
-                      }
-                    },
-                    icon: Icon(Icons.add)),
+                        },
+                        icon: Icon(Icons.add)),
               ],
             )),
         (widget.content?.modules.isEmpty ?? true)
