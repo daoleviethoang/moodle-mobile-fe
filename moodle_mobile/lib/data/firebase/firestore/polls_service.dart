3732cea524851de5de9b6ced3cb4d28ce4132c47
Phat <dinhphat611@gmail.com>
2022-07-24 18:41:40 +0700

    Update polls_service.dart

--- moodle_mobile/lib/data/firebase/firestore/polls_service.dart
+++ moodle_mobile/lib/data/firebase/firestore/polls_service.dart
@@ -1,14 +1,8 @@
-import 'dart:convert';
-
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:flutter/foundation.dart';
-import 'package:http/http.dart' as http;
+
 import 'package:moodle_mobile/data/firebase/constants/collections.dart';
 import 'package:moodle_mobile/data/firebase/firebase_helper.dart';
-import 'package:moodle_mobile/data/network/constants/endpoints.dart';
-import 'package:moodle_mobile/models/imgur/imgur_auth_token.dart';
-import 'package:moodle_mobile/models/imgur/imgur_image.dart';
-import 'package:moodle_mobile/models/imgur/imgur_token.dart';
+
 import 'package:moodle_mobile/models/poll/poll.dart';
 
 class PollService {
@@ -24,5 +18,26 @@ class PollService {
   static Future setPoll(String courseId, Poll poll) async {
     await _db.collection(Collections.polls).doc(courseId).set(poll.toJson());
     print('test2');
+  }
+
+  static Future votePoll(String courseId, String userID, int optionId) async {
+    Poll? temp = await getPollByCouseId(courseId);
+    //Check old id
+    final results = temp!.results ?? {};
+    for (int i = 0; i < temp.options!.length; i++) {
+      results['$i'] ??= [];
+      results['$i']!.removeWhere((element) => element == userID);
+    }
+    //add new id
+    results['$optionId']!.add(userID);
+    //update new
+    await _db
+        .collection(Collections.polls)
+        .doc(courseId)
+        .update({'results': results});
+  }
+
+  static Future deletePoll(String courseId) async {
+    await _db.collection(Collections.polls).doc(courseId).delete();
   }
 }
